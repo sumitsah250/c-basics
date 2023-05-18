7009343376227659a8ef6b55df411dc169c58872
sumitsah <sumitsah250@gmail.com>
2023-05-18 08:43:19 +0545

    day 15

---
+++ xlx.to read n numbers form the user and count prime number in it.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+struct human
+{
+    int roll;
+    char s[20];
+    float marks;
+};
+   
+
+  struct human p[20];
+   p[0].roll=12;
+
+{
+    struct student
+    {
+
+        int roll;
+        char name[20];
+    }r;
+    struct student s;
+    printf("enter the first student data:");
+    printf("enter the roll no and name ?");
+    scanf("%d%s",&s.roll,&s.name);
+    printf("second student data:");
+    printf("enter the roll no:");
+    scanf("%d",&r.roll);
+    printf("enter the name :");
+    scanf("%s",&r.name);
+}
